92a0594685e3bd2a4b13b3a8cbac3237cf8d67dd
Matheus Oliveira <cruz.matheus99@gmail.com>
2022-06-15 14:16:17 -0300

    Aplicativo finalizado.

---
+++ SuperSenha/SecurityTipsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SecurityTipsViewController.swift
+//  SuperSenha
+//
+//  Created by Matheus Oliveira on 01/06/22.
+//
+
+import UIKit
+
+class SecurityTipsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func close(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+}
